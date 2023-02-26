2164de01560b10b6f1eb43891b117b987d5d6895
Patrick Lindsay <lindsay.patrick@student.greenriver.edu>
2023-02-26 14:31:23 -0800

    Added page to create/edit footer links

---
+++ views/admin_footer_links.php
@@ -0,0 +1,26 @@
+<?php
+
+// Link Data should be gotten from datalayer via controller
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Edit Footer Links</title>
+</head>
+<body>
+    <!--NAVBAR-->
+    <include href="views/includes/navbar.html"></include>
+    <div class="row justify-content-center">
+        <div class="col text-center">
+            <h1 class="pt-5">Edit Footer Links</h1>
+            <hr class="shadow-sm">
+        </div>
+    </div>
+
+</body>
+</html>
