1a30402df651beb744dfce583486b5b2428df39f
menchaca-cherie <menchaca.cherie@student.greenriver.edu>
2023-03-20 14:51:07 -0700

    changed the print.js

---
+++ scripts/print.js
@@ -0,0 +1,3 @@
+document.getElementById("printBtn").onclick = () => {
+    window.print();
+};
--- views/includes/print.php
+++ views/includes/print.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($token)){
     echo
-            '<button onclick="window.print()" class="nav-item active float-right ">
+            '<button id="printBtn" class="nav-item active float-right ">
                 <span id="'.$GLOBALS['PROJECT_DIR'].$token.'" hidden></span>
                 <img src="'.$GLOBALS['PROJECT_DIR'].'/images/printer.png" width="100px" height="90px">
             </button>'
